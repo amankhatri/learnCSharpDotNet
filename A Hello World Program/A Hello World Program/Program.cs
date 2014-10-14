20d47d980e1c5f74ebb6867634da63fb1415f13e
amankhatri <aman_khatri5b5@yahoo.com>
2014-10-14 11:07:28 -0500

    Namespace System.Console.writeLine();

---
+++ A Hello World Program/A Hello World Program/Program.cs
@@ -0,0 +1,16 @@
+﻿/*The first line here is called namespace. It tells the program that we will use the code present in system name space
+ */
+/*Namespace is collection of variety fo things. It is a collection of classes, interfaces, structs, enums and delegates.
+ A namespace can contain another name space*/
+using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*we are using console class present in the system namespace.*/
+            Console.WriteLine("Hello World 1");
+            /*you can also do this: */
+            System.Console.WriteLine("Hello World 2");
+            Console.ReadLine();
+        }
+    }
