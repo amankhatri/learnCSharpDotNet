377c49400d3a84bc0affc81aac0f67d70f25efa0
amankhatri <aman_khatri5b5@yahoo.com>
2014-10-20 12:51:11 -0500

    XML style comments are useful for documentation

---
+++ Comments/Comments/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    ///<summary>
+    /// This is a Sample Class and a sample documentation using XML
+    ///</summary>
+    public class SampleClass
+    {
+    }
+
+}
