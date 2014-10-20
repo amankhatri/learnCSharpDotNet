8210dd562e66995fbc50c86b3be4b3d369c870c6
amankhatri <aman_khatri5b5@yahoo.com>
2014-10-20 13:21:49 -0500

    while loop no spacing between <= or any condition

---
+++ while loop/while loop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace while_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*goal print even numbers upto a given target*/
+		    System.Console.WriteLine("Please Enter your target?");
+		    /*System.Console.ReadLine reads the input arguments as strings so we need to parse*/
+		    int userInput = int.Parse(System.Console.ReadLine());
+		    int start = 0;
+		    while(start <= userInput)
+            {
+		        System.Console.WriteLine(start);
+		        start = start +2;
+            }
+}
