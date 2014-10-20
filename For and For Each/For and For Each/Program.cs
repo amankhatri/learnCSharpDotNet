9302c3c7cb8a914509f55e72e05da804da696b33
amankhatri <aman_khatri5b5@yahoo.com>
2014-10-20 14:00:58 -0500

    for(datatype variable in collection) foreach while

---
+++ For and For Each/For and For Each/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_and_For_Each
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[3];
+            numbers[0] = 101;
+            numbers[1] = 102;
+            numbers[2] = 103;
+            int i = 0;
+            while (i < numbers.Length)
+            {
+                System.Console.WriteLine(numbers[i]);
+                i++;
+            }
+            for (int j = 0; j < numbers.Length; j++)
+            {
+                System.Console.WriteLine(numbers[i]);
+            }
+            foreach (int value in numbers)
+            {
+                System.Console.WriteLine(value);
+            }
+            for (int j = 0; j < numbers.Length; j++)
+            {
+                if (j % 2 == 1)
+                {
+                    /*after the program encounters this word it exits this if condiion and increments j and executes the for loop again*/
+                    continue;
+                }
+                System.Console.WriteLine(numbers[j]);
+            }
+
+        }
+    }
+}
