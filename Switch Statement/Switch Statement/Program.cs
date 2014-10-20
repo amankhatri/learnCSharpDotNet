17303a063c3388f8c505f7fd19e046b4d2055851
amankhatri <aman_khatri5b5@yahoo.com>
2014-10-20 13:09:17 -0500

    Falling through cases

---
+++ Switch Statement/Switch Statement/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Enter a Number");
+		    int userInput = int.Parse(System.Console.ReadLine());
+		    switch(userInput){
+			    case 10:
+				    System.Console.WriteLine("Your Number is 10");
+			        break;
+			    case 20:
+				    System.Console.WriteLine("Your Number is 20");
+			        break;
+	        /*You can also join multple switch conditions and it falls from one condition to another*/
+			    case 30:
+			    case 40:
+			    case 50:
+				    System.Console.WriteLine("Your Number is {0}", userInput);
+                    break;
+			    default:
+				    System.Console.WriteLine("Default number is neither 10 nor 20");
+                    break;
+		}
+        }
+    }
+}
