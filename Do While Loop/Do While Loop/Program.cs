3021e06437925aac7fe35905def9593afe71f4be
amankhatri <aman_khatri5b5@yahoo.com>
2014-10-21 11:46:33 -0500

    Do while be careful while using && and ||

---
+++ Do While Loop/Do While Loop/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Do_While_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userChoice = "";
+            do 
+            {
+                int start=0;
+                System.Console.WriteLine("Please Enter a target number");
+                int userInput = int.Parse(System.Console.ReadLine());
+                while(start<= userInput){
+                    System.Console.WriteLine("Start: "+start);
+                    start = start + 2;
+                }
+               
+                do
+                {
+                    System.Console.WriteLine("Do you want to continue - yes or no? ");
+                    userChoice = System.Console.ReadLine();
+                    if (userChoice!= "yes" && userChoice!= "no")
+                    {
+                        System.Console.WriteLine("Invalid choice. please say yes or no");
+                    }
+                } while (userChoice!= "yes" && userChoice!= "no");
+            } while (userChoice == "yes");
+            System.Console.WriteLine("Done!");
+        }
+    }
+}
