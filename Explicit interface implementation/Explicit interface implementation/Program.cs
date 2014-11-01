9a4bfff0f6edd97c7372b74924caa20ed956d156
amankhatri <aman_khatri5b5@yahoo.com>
2014-11-01 14:31:46 -0500

    interfaces, explicit non explicit , default interface

---
+++ Explicit interface implementation/Explicit interface implementation/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explicit_interface_implementation
+{
+    
+    interface I1
+    {
+        void InterfaceMethod(); //explicit interface can not have access method
+    }
+    interface I2
+    {
+        void InterfaceMethod(); //explicit interface can not have access method
+    }
+    
+    
+    class Program : I1, I2
+    {
+
+        static void Main(string[] args)
+        {
+            Program P1 = new Program();
+            ((I1)P1).InterfaceMethod();
+            ((I2)P1).InterfaceMethod();
+            /*you can also do this:*/
+            I1 i1 = new Program();
+            I2 i2 = new Program();
+            i1.InterfaceMethod();
+            i2.InterfaceMethod();
+            /*if you want to make InterfaceMethod(); from one of the interfaces to be default then you call interfacemethod which you want to make as
+             default like this:*/
+            P1.InterfaceMethod();
+            /*where as non default method should be called explicityly*/
+            System.Console.ReadLine();
+           
+        }
+        /*dot net is thinking that you have implemented the methods from both the classes. Since we cannot distingh beween I1 and I2 which is called
+         * to impelement InterfaceMethod(). To distinguish between the interfaces with same methods we use explicit interfaces*/
+         public void InterfaceMethod() //explicit interface do not have access modifiers. 
+        {
+            System.Console.WriteLine("I1 Interface Method");
+        }
+        /*Explicity interfaces do not have access modifiers 
+         * and you have to use interface name .method name*/
+        void I1.InterfaceMethod() //explicit interface do not have access modifiers. 
+        {
+            System.Console.WriteLine("I1 Interface Method");
+        }
+        void I2.InterfaceMethod() //explicit interface do not have access modifiers. 
+        {
+            System.Console.WriteLine("I2 Interface Method");
+        }
+    }
+}
---
+++ Interfaces/Interfaces/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    /*We create interfaces using interface keyword. Just like classes interfaces also contains properties, moethods, delegates or events, but only 
+     declarations and no implementations. 
+     It is a complie time error to provide implementations for any interface member. Interface memers are public by default and they don't allow explicit access modifiers.
+     Interfaces cannot contain fields. If a class or a struct inherits from an interface. it must provide implementation for all interface members. Otherwise, we get a compiler 
+     error
+     A class or a struct can inherit from more than one interface at the same time, but wjhere as, a class cannot inherit from more than once class at the
+     same time.
+     Interfaces can inherit from other interfaces. A class that inherits this interface must provide implementation for all interface members in the entire
+     interface inheritance chain.
+     we can not create an instance of an interface, but an interface reference variable can point to a derivied class object.
+         interface naming convention: interface names are prefixed with capital I*/
+   
+    interface ICustomer1
+    {
+        /*on inteface methods you can not have access modifiers, Interface can not have feilds like int Id.*/
+        void Print1();
+
+    }
+    interface ICustomer2 : ICustomer1
+    {
+        void Print2();
+    }
+
+    public class Customer : ICustomer2
+    {
+
+        public void Print2()
+        {
+            System.Console.WriteLine("method from ICustomer2");
+        }
+
+        public void Print1()
+        {
+            System.Console.WriteLine("method from ICustomer");
+        }
+    }
+
+    /*a class cannot inherit from another class.*/
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer c1 = new Customer();
+            c1.Print1();
+            c1.Print2();
+            /*You cannot do this ICustomer1 cust = new Icustomer1(); if you do Customer c1 = new Customer(); c1 is an object on heap and
+             everytime you do c1. something, it refers to c1 object on heap. However you can do this:*/
+            ICustomer1 cust = new Customer(); /*As we know that a parent class object can refer to the child class object.*/
+            cust.Print1();
+            System.Console.ReadLine();
+
+        }
+    }
+}
