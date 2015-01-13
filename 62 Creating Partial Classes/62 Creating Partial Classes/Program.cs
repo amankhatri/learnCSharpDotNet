2efe2ef968a2dfd6b7a2e73799427d72123c56e2
amankhatri <aman_khatri5b5@yahoo.com>
2015-01-13 16:59:17 -0600

    using multiple class files to make a single class f
    /*1) All the parts spread across different files must use the partial
    keywords.
    2) All the parts spread across different files, must have the same
    access modifiers, that is we can either have public or internal.
    3) If any of the parts are declared abstract, then the entire type is
    considered abstract.
    4) if any of the parts are declared sealed, then the entire type is
    considered sealed. (Sealed means no class can inherit from a sealed
    class
    5) If any of the parts inherit a class, then the entire type inherits
    that class.
    6) C# doesnt nsupport multiple class inheritance. Different parts of the
    partial class, must not specify different base classes.
    7) Different parts of the partial class can specify different base
    interfaces, and the final type implementas all of the interfaces listed
    by all
    the interfaces listed by all of the partial declaration.
    8) Any memebers that are declared in a partial definition are available
    to all of the other parts of the partial class.*/

---
+++ 62 Creating Partial Classes/62 Creating Partial Classes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _62_Creating_Partial_Classes
+{
+    /*1) All the parts spread across different files must use the partial keywords.
+     2) All the parts spread across different files, must have the same access modifiers, that is we can either have public or internal. 
+     3) If any of the parts are declared abstract, then the entire type is considered abstract.
+     4) if any of the parts are declared sealed, then the entire type is considered sealed. (Sealed means no class can inherit from a sealed class
+     5) If any of the parts inherit a class, then the entire type inherits that class.
+     6) C# doesnt nsupport multiple class inheritance. Different parts of the partial class, must not specify different base classes.
+     7) Different parts of the partial class can specify different base interfaces, and the final type implementas all of the interfaces listed by all
+     the interfaces listed by all of the partial declaration.
+     8) Any memebers that are declared in a partial definition are available to all of the other parts of the partial class.*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ 62 Creating Partial Classes/62 Creating Partial Classes/SamplePartialClassTwo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _62_Creating_Partial_Classes
+{
+    public partial class SamplePartialClass
+    //internal partial class SamplePartialClass
+    { 
+    }
+}
