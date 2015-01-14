dbc79078b8a17e15064639bb09ccb121f714c464
amankhatri <aman_khatri5b5@yahoo.com>
2015-01-13 18:27:29 -0600

    indexers are key

---
+++ 64 How and Where are indexers used/64 How and Where are indexers used/Company.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _64_How_and_Where_are_indexers_used
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+    }
+    class Company
+    {
+        private List<Employee> listEmployees;
+        public Company()
+        {
+            listEmployees = new List<Employee>();
+            listEmployees.Add(new Employee { EmployeeID = 1, Name = "Mike1", Gender = "Male"});
+            listEmployees.Add(new Employee { EmployeeID = 2, Name = "Mike2", Gender = "Female" });
+            listEmployees.Add(new Employee { EmployeeID = 3, Name = "Mike3", Gender = "Male" });
+            listEmployees.Add(new Employee { EmployeeID = 4, Name = "Mike4", Gender = "Male" });
+            listEmployees.Add(new Employee { EmployeeID = 5, Name = "Mike5", Gender = "Female" });
+            listEmployees.Add(new Employee { EmployeeID = 6, Name = "Mike6", Gender = "Male" });
+            listEmployees.Add(new Employee { EmployeeID = 7, Name = "Mike6", Gender = "Male" });
+            listEmployees.Add(new Employee { EmployeeID = 8, Name = "Mike7", Gender = "Male" });
+        }
+        public string this[int employeeId]
+        {
+            get
+            {
+                /*from the list of employee return the first or Defualt entry emp such that (=>) that employeeId (emp.Employee)
+                 * should match whatever (==) we pass into this indexer (employeeId)  to return an employee object. From that object pickup their
+                 name and give me name*/
+                return listEmployees.FirstOrDefault(emp => emp.EmployeeID == employeeId).Name;
+            }
+            set
+            {
+                listEmployees.First(emp => emp.EmployeeID == employeeId).Name = value; ;
+            }
+        }
+    }
+}
---
+++ 64 How and Where are indexers used/64 How and Where are indexers used/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _64_How_and_Where_are_indexers_used
+{
+    class Program
+    {
+        /*Indexers are keys which can be strings or integers. INdexers allow instances of class to be indexed just like arrays
+         Use this keyword to creawte an indexer
+         This indexer takes emploteeId as parameter
+          and returns employee name;*/
+        static void Main(string[] args)
+        {
+            Company company = new Company();
+           System.Console.WriteLine("Name of Employeee with id = 2" +company[2]);
+           System.Console.WriteLine("Chaning the name of the employess with id = 2, 5, and 8");
+           company[2] = "Jake2";
+           company[5] = "Jake5";
+           company[8] = "Jake8";
+           System.Console.WriteLine("Name of Employeee with id = 2" + company[2]);
+           System.Console.WriteLine("Name of Employeee with id = 5" + company[5]);
+           System.Console.WriteLine("Name of Employeee with id = 8" + company[8]);
+
+        }
+    }
+}
