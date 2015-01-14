bc7fc3c3109c758a4e7e26e69a37013754e92560
amankhatri <aman_khatri5b5@yahoo.com>
2015-01-14 17:33:24 -0600

    indexers and how to implement them

---
+++ 66 Overloading Indexers/66 Overloading Indexers/Company.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _66_Overloading_Indexers
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+    }
+    class Company
+    {
+        private List<Employee> listEmployees;
+        public Company()
+        {
+            listEmployees = new List<Employee>();
+            listEmployees.Add(new Employee { EmployeeID = 1, Name = "Mike1", Gender = "Male" });
+            listEmployees.Add(new Employee { EmployeeID = 2, Name = "Mike2", Gender = "Female" });
+            listEmployees.Add(new Employee { EmployeeID = 3, Name = "Mike3", Gender = "Male" });
+            listEmployees.Add(new Employee { EmployeeID = 4, Name = "Mike4", Gender = "Male" });
+            listEmployees.Add(new Employee { EmployeeID = 5, Name = "Mike5", Gender = "Female" });
+            listEmployees.Add(new Employee { EmployeeID = 6, Name = "Mike6", Gender = "Male" });
+            listEmployees.Add(new Employee { EmployeeID = 7, Name = "Mike6", Gender = "Male" });
+            listEmployees.Add(new Employee { EmployeeID = 8, Name = "Mike7", Gender = "Male" });
+        }
+        public string this[int employeeId]
+        {
+            get
+            {
+                /*from the list of employee return the first or Defualt entry emp such that (=>) that employeeId (emp.Employee)
+                 * should match whatever (==) we pass into this indexer (employeeId)  to return an employee object. From that object pickup their
+                 name and give me name*/
+                return listEmployees.FirstOrDefault(emp => emp.EmployeeID == employeeId).Name;
+            }
+            set
+            {
+                listEmployees.First(emp => emp.EmployeeID == employeeId).Name = value; ;
+            }
+        }
+        public string this[string Gender]
+        {
+            get
+            {
+                /*from the list of employee return the first or Defualt entry emp such that (=>) that employeeId (emp.Employee)
+                 * should match whatever (==) we pass into this indexer (employeeId)  to return an employee object. From that object pickup their
+                 name and give me name*/
+                return listEmployees.Count(emp => emp.Gender == Gender).ToString();
+            }
+            set
+            {
+                foreach (Employee employee in listEmployees)
+                {
+                    if (employee.Gender == Gender) { employee.Gender = value; }
+                }
+            }
+        }
+    }
+}
---
+++ 66 Overloading Indexers/66 Overloading Indexers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _66_Overloading_Indexers
+{
+    class Program
+    {
+        /*Indexers are keys which can be strings or integers. INdexers allow instances of class to be indexed just like arrays
+         Use this keyword to creawte an indexer
+         This indexer takes emploteeId as parameter
+          and returns employee name;*/
+        static void Main(string[] args)
+        {
+            Company company = new Company();
+            System.Console.WriteLine("Name of Employeee with id = 2" + company[2]);
+            System.Console.WriteLine("Chaning the name of the employess with id = 2, 5, and 8");
+            company[2] = "Jake2";
+            company[5] = "Jake5";
+            company[8] = "Jake8";
+            System.Console.WriteLine("Name of Employeee with id = 2" + company[2]);
+            System.Console.WriteLine("Name of Employeee with id = 5" + company[5]);
+            System.Console.WriteLine("Name of Employeee with id = 8" + company[8]);
+            System.Console.WriteLine("Total Male employee " + company["Male"]);
+            System.Console.WriteLine("Total Female employee " + company["Female"]);
+            company["Male"] = "Female";
+            System.Console.WriteLine("After changes");
+            System.Console.WriteLine("Total Male employee " + company["Male"]);
+            System.Console.WriteLine("Total Female employee " + company["Female"]);
+        }
+    }
+}
