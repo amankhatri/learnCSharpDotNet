6a7f9a51a443f37a20a5bb04d6a0268bda92cd1c
amankhatri <aman_khatri5b5@yahoo.com>
2015-01-14 17:50:26 -0600

    make two functions  of same name and pass null
    /*using method overloading to make method parameters optional*/
    static void Main(string[] args)
    {
    AddNumbers(10, 20, new int[]{30,40});
    }
    public static void AddNumbers(int firstNumber, int secondNumber)
    {
    AddNumbers(firstNumber, secondNumber, null);
    }
    public static void AddNumbers(int firstNumber, int secondNumber, int[]
    restOfNumbers)
    {
    int result = firstNumber + secondNumber;
    if(restOfNumbers != null)
    {
    foreach (int i in restOfNumbers)
    {
    result = result + i;
    }
    }
    System.Console.WriteLine("Sum " + result);
    }

---
+++ 68 Making Method Parameters optional/68 Making Method Parameters optional/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _68_Making_Method_Parameters_optional
+{
+    class Program
+    {
+        /*using method overloading to make method parameters optional*/
+        static void Main(string[] args)
+        {
+            AddNumbers(10, 20, new int[]{30,40});
+        }
+        public static void AddNumbers(int firstNumber, int secondNumber)
+        {
+            AddNumbers(firstNumber, secondNumber, null);
+        }
+        public static void AddNumbers(int firstNumber, int secondNumber, int[] restOfNumbers)
+        {
+            int result = firstNumber + secondNumber;
+            if(restOfNumbers != null)
+            {
+                foreach (int i in restOfNumbers)
+                {
+                    result = result + i;
+                }
+            }
+            System.Console.WriteLine("Sum " + result);
+        }
+    }
+}
