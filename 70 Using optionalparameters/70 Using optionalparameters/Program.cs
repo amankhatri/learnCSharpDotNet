b6645c37e8123a7e95db8ce8089f17eea96516a0
amankhatri <aman_khatri5b5@yahoo.com>
2015-01-14 18:07:18 -0600

    /*optional parameter using optionalAttribute*/
    /*optional parameter using optionalAttribute*/
    public static void AddNumbers(int firstNumber, int secondNumber,
    [Optional] int[] restOfNumbers = null)
    {
    int result = firstNumber + secondNumber;
    if (restOfNumbers != null)
    {
    foreach (int i in restOfNumbers)
    {
    result = result + i;
    }
    }
    System.Console.WriteLine("Sum " + result);
    }

---
+++ 70 Using optionalparameters/70 Using optionalparameters/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+
+namespace _70_Using_optionalparameters
+{
+    class Program
+    {
+        /*optional parameter using optionalAttribute*/
+        static void Main(string[] args)
+        {
+            AddNumbers(10, 20);
+        }
+        /*optional parameter using optionalAttribute*/
+        public static void AddNumbers(int firstNumber, int secondNumber, [Optional] int[] restOfNumbers = null)
+        {
+            int result = firstNumber + secondNumber;
+            if (restOfNumbers != null)
+            {
+                foreach (int i in restOfNumbers)
+                {
+                    result = result + i;
+                }
+            }
+            System.Console.WriteLine("Sum " + result);
+        }
+   
+       
+    }
+}
